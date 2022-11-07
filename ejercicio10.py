9250ed0be23c16b262a7349b709d1b04e1a1a7a8
Inés <al_ijimenezg@iessierradelasvillas.org>
2022-11-07 10:53:06 +0100

    10&11

--- ejercicio10.py
+++ ejercicio10.py
@@ -2,6 +2,7 @@
 Ejercicio 10
 Algoritmo que muestre la tabla de multiplicar de los números 1,2,3,4 y 5.
 '''
+num=0
 
 for num in range (1,6):
     print(f"{num}:")
---
+++ ejercicio11.py
@@ -0,0 +1,20 @@
+'''
+Ejercicio 11
+Escribe un programa que diga si un número introducido por teclado es o no
+primo. Un número primo es aquel que sólo es divisible entre él mismo y la
+unidad. Nota: Es suficiente probar hasta la raíz cuadrada del número para ver si
+es divisible por algún otro número.
+'''
+
+num=int(input("Di un numero:"))
+raiz=int(num**(1/2))
+divisible=0
+
+for numero in range (1, raiz+1):
+    if(num%numero==0):
+        divisible+=1
+
+if (divisible==1):
+    print (f"{num} es primo")
+else:
+    print(f"{num} no es primo")
