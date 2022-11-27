a15aaa77a250f8be60f8e5aeba82671b76ac389e
Inés Jiménez <al_ijimenezg@iessierradelasvillas.org>
2022-11-27 02:29:25 +0100

    editado 19

--- ejercicio19.py
+++ ejercicio19.py
@@ -4,14 +4,13 @@ Realizar un ejemplo de menú, donde podemos escoger las distintas opciones
 hasta que seleccionamos la opción de “Salir”.
 '''
 def menu (num:int):
-
-    while (num!=5):
+    while (num!=4):
         print("1.Ver contactos")
         print("2.Añadir contactos")
         print("3.Eliminar contactos")
         print("4.Salir")
         num=int(input("Seleccionar menu: "))
-
+    
     return(num)
 
 numero=2
